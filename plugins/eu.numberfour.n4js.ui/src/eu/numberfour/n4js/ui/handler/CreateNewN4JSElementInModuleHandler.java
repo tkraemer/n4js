25d8f790419d0693a8a9d5c22ed8b220cac8d54a
Luca Beurer-Kellner <luca.beurer-kellner@numberfour.eu>
2016-03-23 14:03:16 +0100

    IDE-2157: error dialog + plugin.xml formatting

--- plugins/eu.numberfour.n4js.ui/src/eu/numberfour/n4js/ui/handler/CreateNewN4JSElementInModuleHandler.java
+++ plugins/eu.numberfour.n4js.ui/src/eu/numberfour/n4js/ui/handler/CreateNewN4JSElementInModuleHandler.java
@@ -18,11 +18,14 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWizard;
@@ -45,8 +48,48 @@ public class CreateNewN4JSElementInModuleHandler extends AbstractHandler {
 	private static final String WIZARD_ID_PARAMETER_ID = "eu.numberfour.n4js.ui.wizard.CreateNewN4JSElementInModule.wizardId";
 	private static final String NESTED_PARAMETER_ID = "eu.numberfour.n4js.ui.wizard.CreateNewN4JSElementInModule.nested";
 
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		String wizardId = event.getParameter(WIZARD_ID_PARAMETER_ID);
+		String nested = event.getParameter(NESTED_PARAMETER_ID);
+
+		boolean isNested = false;
+
+		if (wizardId == null) {
+			return null;
+		}
+
+		if (nested != null) {
+			isNested = nested.equals("true");
+		}
+
+		IStructuredSelection selection;
+
+		// Try to get editor file
+		IFile editorFile = getOpenEditorFile();
+
+		if (null != editorFile) {
+			selection = new StructuredSelection(editorFile);
+		} else {
+			// Try to get the selection from the project explorer tree view
+			IResource resourceTreeFile = getTreeSelection();
+			if (null != resourceTreeFile) {
+				// If it's a tree selection set nested to false
+				isNested = false;
+				selection = new StructuredSelection(resourceTreeFile);
+			} else {
+				// If not selection can be retrieved, launch the wizard with an empty selection
+				selection = new StructuredSelection();
+			}
+		}
+
+		openWizardForModule(wizardId, selection, isNested);
+
+		return null;
+	}
+
 	/**
-	 * Returns the file which is currently opened in the active editor.
+	 * Returns the file which is currently open in the active editor.
 	 *
 	 * Returns null if no editor is open.
 	 */
@@ -88,52 +131,17 @@ public class CreateNewN4JSElementInModuleHandler extends AbstractHandler {
 		return null;
 	}
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		String wizardId = event.getParameter(WIZARD_ID_PARAMETER_ID);
-		String nested = event.getParameter(NESTED_PARAMETER_ID);
-
-		boolean isNested = false;
-
-		if (wizardId == null) {
-			return null;
-		}
-
-		if (nested != null) {
-			isNested = nested.equals("true");
-		}
-
-		IStructuredSelection selection;
-
-		// Try to get editor file
-		IFile editorFile = getOpenEditorFile();
-		// Try to get the selection from the project explorer tree view
-		IResource resourceTreeFile = getTreeSelection();
-
-		if (null != editorFile) {
-			selection = new StructuredSelection(editorFile);
-		} else if (null != resourceTreeFile) {
-			// If it's a tree selection set nested to false to stay consistent
-			isNested = false;
-			selection = new StructuredSelection(resourceTreeFile);
-		} else {
-			selection = new StructuredSelection();
-		}
-
-		openWizardForModule(wizardId, selection, isNested);
-
-		return null;
-	}
-
 	/**
-	 * Opens the wizard with the given id and passes it the module as initial selection.
+	 * Opens the wizard with the given id and passes it the selection.
 	 *
 	 * @param wizardId
 	 *            The wizard id of the eclipse newWizard registry
 	 * @param selection
-	 *            The module resource
+	 *            The selection
 	 */
 	private void openWizardForModule(String wizardId, IStructuredSelection selection, boolean nested) {
+
+		// Retrieve wizard from registry
 		IWizardDescriptor wizardDescriptor = PlatformUI.getWorkbench().getNewWizardRegistry().findWizard(wizardId);
 
 		if (wizardDescriptor == null) {
@@ -162,6 +170,9 @@ public class CreateNewN4JSElementInModuleHandler extends AbstractHandler {
 
 		} catch (CoreException e) {
 			/** Failed to create the wizard */
+			Shell workbenchShell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+			MessageDialog.open(MessageDialog.ERROR, workbenchShell, "Failed to launch wizard",
+					String.format("Failed to launch wizard %s", wizardId), SWT.SHEET);
 			return;
 		}
 
