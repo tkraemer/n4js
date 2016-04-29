c65c4f327a553ac79abffe93359ec0daa19ace21
Akos Kitta <akos.kitta@numberfour.eu>
2016-04-29 17:16:24 +0200

    GH-180: Provided a generic injection support per plug-ins.

---
+++ tests/eu.numberfour.n4js.ui.tests/src/eu/numberfour/n4js/tests/bugs/GH_180_CheckInjectedSharedSingletons_PluginUITest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2016 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package eu.numberfour.n4js.tests.bugs;
+
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+
+import eu.numberfour.n4js.typeinference.N4JSTypeInferencer;
+import eu.numberfour.n4js.ui.internal.N4JSActivator;
+import eu.numberfour.n4js.utils.Arrays2;
+import eu.numberfour.n4js.utils.injector.AbstractGuiceUIPlugin;
+
+/**
+ * Class for checking whether singleton instances are shared among parent-child injectors.
+ */
+public class GH_180_CheckInjectedSharedSingletons_PluginUITest extends AbstractIDEBUG_Test {
+
+	@Override
+	protected ProjectImporter getProjectImporter() {
+		return ProjectImporter.NOOP;
+	}
+
+	/**
+	 * Checks whether the same instance is provided by different injector instances when the class of the provided
+	 * instance is annotated with {@code @Singleton}.
+	 */
+	@Test
+	public void testInjectorsSharingSameSingleton() throws Exception {
+
+		final Class<N4JSTypeInferencer> testedType = N4JSTypeInferencer.class;
+		final Singleton[] singletons = testedType.getAnnotationsByType(Singleton.class);
+		assertTrue(testedType.getSimpleName() + " is not annotated with " + Singleton.class.getName() + ".",
+				!Arrays2.isEmpty(singletons));
+
+		final String injectorId = N4JSActivator.EU_NUMBERFOUR_N4JS_N4JS;
+		final Injector parentInjector = N4JSActivator.getInstance().getInjector(injectorId);
+		final AbstractGuiceUIPlugin mockBundle = new AbstractGuiceUIPlugin() {
+
+			@Override
+			protected Injector getParentInjector(final String id) {
+				return parentInjector;
+			}
+
+			@Override
+			protected void doStart(BundleContext context) throws Exception {
+				// Nothing.
+			}
+
+			@Override
+			protected void doStop(BundleContext context) throws Exception {
+				// Nothing.
+			}
+
+		};
+
+		try {
+			mockBundle.start(/* context */ null);
+			assertTrue("Mock bundle is not running yet.", Bundle.ACTIVE == mockBundle.getBundle().getState());
+			final Injector childInjector = mockBundle.getInjector(injectorId);
+
+			final N4JSTypeInferencer instanceFromChild = parentInjector.getInstance(testedType);
+			final N4JSTypeInferencer instanceFromParent = childInjector.getInstance(testedType);
+
+			assertTrue(
+					"Expected the same instance of " + testedType.getSimpleName() + " from parent and child injectors.",
+					instanceFromChild == instanceFromParent);
+
+		} finally {
+			mockBundle.stop(/* context */ null);
+		}
+	}
+
+}
