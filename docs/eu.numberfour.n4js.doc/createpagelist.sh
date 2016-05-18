13a5a4a15187b2bfa24a6deebd61a352a5d3d8d4
Brian Thomas Smith <brian.smith@numberfour.eu>
2016-05-18 15:48:46 +0200

    Update createpagelist.sh

--- docs/eu.numberfour.n4js.doc/createpagelist.sh
+++ docs/eu.numberfour.n4js.doc/createpagelist.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-find ./target/html -name *.html | cut -sd / -f 1- >help-pages.txt
+echo `pwd`
+if [ -d "./target" ]; then
+  echo "target exists"
+fi
+echo $1
+find target -name *.html | cut -sd / -f 1- >help-pages.txt
