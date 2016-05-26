1c7a5041ba43248e99a82c99a011c7979973e174
Luca Beurer-Kellner <luca.beurer-kellner@numberfour.eu>
2016-05-26 12:01:44 +0200

    GH-47: Populate the type search dialog with the selection value.

--- plugins/eu.numberfour.n4js.ui/src/eu/numberfour/n4js/ui/typesearch/OpenTypeSelectionDialogHandler.java
+++ plugins/eu.numberfour.n4js.ui/src/eu/numberfour/n4js/ui/typesearch/OpenTypeSelectionDialogHandler.java
@@ -15,9 +15,14 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.ui.ISources;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+
+import eu.numberfour.n4js.ui.utils.HandlerServiceUtils;
 
 /**
  * Handler for opening the N4JS type selection dialog.
@@ -33,13 +38,36 @@ public class OpenTypeSelectionDialogHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		if (TYPE_SEARCH_IN_USE.compareAndSet(false, true)) {
 			try {
-				provider.get().open();
+				OpenTypeSelectionDialog typeSelectionDialog = provider.get();
+				typeSelectionDialog.setInitialPattern(computeInitialPattern());
+				typeSelectionDialog.open();
 			} finally {
 				TYPE_SEARCH_IN_USE.set(false);
 			}
 		}
 
 		return null;
+	}
+
+	/**
+	 * Computes and returns the initial pattern for the type search dialog.
+	 *
+	 * If no initial pattern can be computed, an empty string is returned.
+	 */
+	private String computeInitialPattern() {
+		IEvaluationContext currentWorkbenchState = HandlerServiceUtils.getCurrentWorkbenchState().orNull();
+
+		if (null != currentWorkbenchState) {
+			Object activeCurrentSelection = currentWorkbenchState.getVariable(ISources.ACTIVE_CURRENT_SELECTION_NAME);
+
+			if (activeCurrentSelection instanceof TextSelection) {
+				String text = ((TextSelection) activeCurrentSelection).getText();
+				if (null != text && text.length() > 0) {
+					return text;
+				}
+			}
+		}
+		return "";
 	}
 
 }
