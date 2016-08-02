68c97e4b3b10f66ae7b53b3a8fb1cb8ca61f8e5e
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2016-08-02 17:32:34 +0200

    IDE-1852 RegionWithCursor no longer requires duplicating ParameterParser
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@typefox.io>

--- testhelpers/eu.numberfour.n4js.ui.tests.helper/src/eu/numberfour/n4js/xpect/RegionWithCursor.java
+++ testhelpers/eu.numberfour.n4js.ui.tests.helper/src/eu/numberfour/n4js/xpect/RegionWithCursor.java
@@ -10,13 +10,95 @@
  */
 package eu.numberfour.n4js.xpect;
 
-import org.xpect.text.Region;
+import org.xpect.XpectImport;
+import org.xpect.XpectInvocation;
+import org.xpect.parameter.DerivedRegion;
+import org.xpect.parameter.IStatementRelatedRegion;
+import org.xpect.parameter.StringRegion;
+import org.xpect.setup.XpectSetupFactory;
+import org.xpect.state.Creates;
 
 /**
  * Region with global Cursor. The cursor is not necessarily inside of the region.
  */
-public class RegionWithCursor extends Region {
+@XpectImport(RegionWithCursor.Provider.class)
+public class RegionWithCursor extends DerivedRegion {
+
 	private final int globalCursorOffset;
+
+	/***/
+	@XpectSetupFactory
+	public static class Provider {
+
+		private final StringRegion delegate;
+
+		/***/
+		public Provider(StringRegion delegate) {
+			this.delegate = delegate;
+		}
+
+		/***/
+		@Creates
+		public RegionWithCursor createRegionWithCursor() {
+			CursorMarkerHelper cmh = new CursorMarkerHelper();
+			String val = delegate.getRegionText();
+			StringBuffer sb = new StringBuffer(val);
+
+			@SuppressWarnings("unused")
+			boolean hasCursor = CursorMarkerHelper.exists(val, cmh.markerCursor);
+			boolean hasEndSelection = CursorMarkerHelper.exists(val, cmh.markerSelectionEnd);
+			boolean hasStartSelection = CursorMarkerHelper.exists(val, cmh.markerSelectionStart);
+
+			int cursorOffset = CursorMarkerHelper.deleteMarker(sb, cmh.markerCursor);
+			int selStartOffset = CursorMarkerHelper.deleteMarker(sb, cmh.markerSelectionStart);
+			int selEndOffset = CursorMarkerHelper.deleteMarker(sb, cmh.markerSelectionEnd);
+			val = sb.toString();
+
+			// reduce cursor offset if selection markers were inserted in front of.
+			if (hasStartSelection && selStartOffset < cursorOffset)
+				cursorOffset -= cmh.markerSelectionStart.length();
+			if (hasEndSelection && selEndOffset < cursorOffset)
+				cursorOffset -= cmh.markerSelectionEnd.length();
+
+			XpectInvocation invocation = delegate.getStatement();
+
+			IStatementRelatedRegion extendedRegion = invocation.getExtendedRegion();
+			int offset = extendedRegion.getOffset() + extendedRegion.getLength();
+
+			String text = invocation.getFile().getDocument();
+			int result = text.indexOf(val, offset);
+			if (result < 0)
+				throw new RuntimeException("OFFSET '" + val + "' not found");
+
+			int globalCursorOffset = result + cursorOffset;
+			if (hasEndSelection) {
+				if (hasStartSelection) {
+					return new RegionWithCursor(delegate, result + selStartOffset, selEndOffset - selStartOffset,
+							globalCursorOffset);
+				} else {
+					// no startselection, so cursor is startselecction.
+
+					if (selEndOffset < cursorOffset)
+						throw new RuntimeException("OFFSET '" + delegate + " has no selection start("
+								+ cmh.markerSelectionStart + ").");
+					else
+						return new RegionWithCursor(delegate, globalCursorOffset, selEndOffset - cursorOffset,
+								globalCursorOffset);
+				}
+			} else {
+				// No end Selection
+				if (!hasStartSelection) {
+					// not start & no end -> empty
+					return new RegionWithCursor(delegate, globalCursorOffset);
+				} else {
+					// start but no end selection -> error
+					throw new RuntimeException("OFFSET '" + delegate + " has no end selection marker ("
+							+ cmh.markerSelectionEnd + ").");
+				}
+			}
+		}
+
+	}
 
 	/**
 	 * @param offset
@@ -26,8 +108,8 @@ public class RegionWithCursor extends Region {
 	 * @param globalCursorOffset
 	 *            cursor position
 	 */
-	public RegionWithCursor(int offset, int lenght, int globalCursorOffset) {
-		super(offset, lenght);
+	public RegionWithCursor(IStatementRelatedRegion origin, int offset, int lenght, int globalCursorOffset) {
+		super(origin, offset, lenght);
 		this.globalCursorOffset = globalCursorOffset;
 	}
 
@@ -37,8 +119,8 @@ public class RegionWithCursor extends Region {
 	 * @param globalCursorOffset2
 	 *            position
 	 */
-	public RegionWithCursor(int globalCursorOffset2) {
-		this(globalCursorOffset2, 0, globalCursorOffset2);
+	public RegionWithCursor(IStatementRelatedRegion origin, int globalCursorOffset2) {
+		this(origin, globalCursorOffset2, 0, globalCursorOffset2);
 	}
 
 	/**
