07338b1e8e50918ccc8ffe73302a0b0ae160821c
Kristian Duske <kristian.duske@numberfour.eu>
2016-08-25 10:23:18 +0200

    GH-40: Add SimpleParser class to utils and use it for the CSV parser.

--- testhelpers/eu.numberfour.n4js.tests.helper/src/eu/numberfour/n4js/csv/CSVRecord.java
+++ testhelpers/eu.numberfour.n4js.tests.helper/src/eu/numberfour/n4js/csv/CSVRecord.java
@@ -8,86 +8,96 @@
  * Contributors:
  *   NumberFour AG - Initial API and implementation
  */
-package eu.numberfour.n4js.csv
+package eu.numberfour.n4js.csv;
 
-import java.util.Collections
-import java.util.Iterator
-import java.util.List
-import java.util.Objects
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A record of CSV data.
  */
 public class CSVRecord implements Iterable<String> {
-	private List<String> record;
-	
+	private final List<String> record;
+
 	/**
 	 * Creates a new empty CSV record.
 	 */
-	protected new() {
-		this.record = newArrayList();
+	protected CSVRecord() {
+		this.record = new ArrayList<>();
 	}
-		
+
 	/**
 	 * Creates a new CSV record with the given data.
-	 * 
-	 * @param record the records
+	 *
+	 * @param record
+	 *            the records
 	 */
-	private new(List<String> record) {
+	private CSVRecord(List<String> record) {
 		this.record = Objects.requireNonNull(record);
 	}
-		
+
 	/**
 	 * Adds the given field to this record.
-	 * 
-	 * @param field the field to add
+	 *
+	 * @param field
+	 *            the field to add
 	 */
-	protected def void add(String field) {
+	protected void add(String field) {
 		record.add(Objects.requireNonNull(field));
 	}
-	
+
 	/**
 	 * Returns the number of fields (columns) in this record.
-	 * 
+	 *
 	 * @return the number of fields in this record
 	 */
-	public def int getSize() {
+	public int getSize() {
 		return record.size();
 	}
-	
+
 	/**
 	 * Returns the field at the given index.
-	 * 
-	 * @param index the index of the field to return
-	 * 
+	 *
+	 * @param index
+	 *            the index of the field to return
+	 *
 	 * @return the field at the given index
-	 * 
-	 * @throws IndexOutOfBoundsException if the given index is out of bounds
+	 *
+	 * @throws IndexOutOfBoundsException
+	 *             if the given index is out of bounds
 	 */
-	public def String get(int index) {
+	public String get(int index) {
 		return record.get(index);
 	}
 
 	/**
 	 * Returns a view of this record with the given parameters.
-	 * 
-	 * @param index the index of the first field to include in the view
-	 * @param count the number of fields to include in the view (pass negative value to include all remaining fields)
-	 * 
+	 *
+	 * @param index
+	 *            the index of the first field to include in the view
+	 * @param count
+	 *            the number of fields to include in the view (pass negative value to include all remaining fields)
+	 *
 	 * @return a view of this record with the given parameters
-	 * 
-	 * @throws IndexOutOfBoundsException if either of the given parameters is out of bounds
+	 *
+	 * @throws IndexOutOfBoundsException
+	 *             if either of the given parameters is out of bounds
 	 */
-	public def CSVRecord getRange(int index, int count) {
-		val int actualCount = if (count < 0) size - index else count;
+	public CSVRecord getRange(int index, int count) {
+		final int actualCount = count < 0 ? getSize() - index : count;
 		return new CSVRecord(record.subList(index, index + actualCount));
 	}
 
-	override public def Iterator<String> iterator() {
+	@Override
+	public Iterator<String> iterator() {
 		return Collections.unmodifiableList(record).iterator();
 	}
-	
-	override toString() {
+
+	@Override
+	public String toString() {
 		return record.toString();
 	}
 }
