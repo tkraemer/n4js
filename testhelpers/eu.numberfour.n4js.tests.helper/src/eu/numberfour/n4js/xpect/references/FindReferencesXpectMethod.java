6b62d19e392f9e9310504698f5fade0a23e0bd11
mmews <marcus.mews@numberfour.eu>
2016-09-28 15:55:29 +0200

    IDE-1662: changed tests and xpect method name

--- testhelpers/eu.numberfour.n4js.tests.helper/src/eu/numberfour/n4js/xpect/references/FindReferencesXpectMethod.java
+++ testhelpers/eu.numberfour.n4js.tests.helper/src/eu/numberfour/n4js/xpect/references/FindReferencesXpectMethod.java
@@ -10,33 +10,21 @@ package eu.numberfour.n4js.xpect.references;
 import java.util.ArrayList;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.findReferences.IReferenceFinder;
 import org.eclipse.xtext.findReferences.TargetURIs;
 import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
-import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider;
 import org.junit.runner.RunWith;
 import org.xpect.XpectImport;
-import org.xpect.XpectInvocation;
-import org.xpect.parameter.OffsetRegion;
 import org.xpect.parameter.ParameterParser;
 import org.xpect.runner.Xpect;
 import org.xpect.runner.XpectRunner;
-import org.xpect.setup.XpectSetupFactory;
-import org.xpect.state.Creates;
 import org.xpect.xtext.lib.setup.XtextStandaloneSetup;
 import org.xpect.xtext.lib.setup.XtextWorkspaceSetup;
-import org.xpect.xtext.lib.util.XtextOffsetAdapter;
-import org.xpect.xtext.lib.util.XtextOffsetAdapter.EAttributeAndEObject;
-import org.xpect.xtext.lib.util.XtextOffsetAdapter.EStructuralFeatureAndEObject;
-import org.xpect.xtext.lib.util.XtextOffsetAdapter.EStructuralFeatureAndEObjectProvider;
-import org.xpect.xtext.lib.util.XtextOffsetAdapter.IEAttributeAndEObject;
 
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
@@ -50,7 +38,7 @@ import eu.numberfour.n4js.xpect.scoping.N4JSCommaSeparatedValuesExpectation;
 @SuppressWarnings("restriction")
 @RunWith(XpectRunner.class)
 @XpectImport({ XtextStandaloneSetup.class, XtextWorkspaceSetup.class })
-public class ReferencesXpectMethod {
+public class FindReferencesXpectMethod {
 
 	@Inject
 	private Provider<TargetURIs> targetURISetProvider;
@@ -61,64 +49,15 @@ public class ReferencesXpectMethod {
 	@Inject
 	private ResourceDescriptionsProvider resourceDescriptionsProvider;
 
-	static class MyXtextOffsetAdapter extends XtextOffsetAdapter {
-		@XpectSetupFactory
-		protected static class MyEStructuralFeatureAndEObjectProvider extends EStructuralFeatureAndEObjectProvider {
-
-			public MyEStructuralFeatureAndEObjectProvider(XtextResource resource, XpectInvocation statement) {
-				super(resource, statement);
-			}
-
-			public MyEStructuralFeatureAndEObjectProvider(XtextResource resource, XpectInvocation statement,
-					OffsetRegion region) {
-				super(resource, statement, region);
-			}
-
-			@Override
-			protected MyEStructuralFeatureAndEObject create(EObject object, EStructuralFeature feat) {
-				return new EAttributeAndEObject(object, feat);
-			}
-
-			@Creates
-			public IEAttributeAndEObject createEAttributeAndEObject() {
-				return (IEAttributeAndEObject) createStructuralFeatureAndEObject();
-			}
-
-			@Override
-			protected boolean matches(EObject object, EStructuralFeature feature) {
-				return feature instanceof EAttribute;
-			}
-		}
-
-		protected static class EStructuralFeatureAndEObject extends EStructuralFeatureAndEObject
-				implements IEStructuralFeatureAndEObject {
-
-			public EAttributeAndEObject(EObject object, EStructuralFeature feature) {
-				super(object, feature);
-			}
-
-			public EAttribute getEAttribute() {
-				return (EAttribute) getEStructuralFeature();
-			}
-
-		}
-
-		@XpectImport(EStructuralFeatureAndEObjectProvider.class)
-		public static interface IEStructuralFeatureAndEObject extends IEObjectOwner {
-			EStructuralFeature getEStructuralFeature();
-		}
-	}
-
 	/**
 	 * Compares scope including resource name and line number.
 	 */
 	@Xpect
 	@ParameterParser(syntax = "('at' arg1=OFFSET)?")
-	public void references( //
-			@N4JSCommaSeparatedValuesExpectation IN4JSCommaSeparatedValuesExpectation expectation, //
-			IEAttributeAndEObject arg1 //
-	) {
-		Resource eResource = arg1.getEObject().eResource();
+	public void findReferences( //
+			@N4JSCommaSeparatedValuesExpectation IN4JSCommaSeparatedValuesExpectation expectation,
+			EObject arg1) {
+		Resource eResource = arg1.eResource();
 		TargetURIs targets = targetURISetProvider.get();
 		IResourceDescriptions index = resourceDescriptionsProvider.getResourceDescriptions(eResource);
 
