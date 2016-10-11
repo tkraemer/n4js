tkraemer/n4js

commit 477c8caf19b6190dc95f7105461e4243310cda54

IDE-1579: fix a bogus short-cut in subtype rule

plugins/eu.numberfour.n4js/src/eu/numberfour/n4js/typesystem/n4js.xsemantics
@@ -1223,8 +1223,8 @@ from {
 	} else if (leftDeclType.eIsProxy || rightDeclType.eIsProxy) {
 		true;
 	} else if (leftDeclType instanceof UndefinedType
-				|| (leftDeclType instanceof NullType && !(rightDeclType instanceof UndefinedType || rightDeclType instanceof VoidType))
-				|| (rightDeclType instanceof AnyType && !(leftDeclType instanceof VoidType))) {
+				|| (leftDeclType instanceof NullType && !(rightDeclType instanceof UndefinedType || rightDeclType instanceof VoidType || rightDeclType instanceof TypeVariable))
+				|| (rightDeclType instanceof AnyType && !(leftDeclType instanceof VoidType || leftDeclType instanceof TypeVariable))) {
 		true
 	} else if (leftDeclType instanceof VoidType
 				|| rightDeclType instanceof VoidType) {

plugins/eu.numberfour.n4js/xsemantics-gen/eu/numberfour/n4js/xsemantics/InternalTypeSystem.java
@@ -3723,7 +3723,7 @@ public class InternalTypeSystem extends XsemanticsRuntimeSystem {
           sneakyThrowRuleFailedException("true");
         }
       } else {
-        if ((((leftDeclType instanceof UndefinedType) || ((leftDeclType instanceof NullType) && (!((rightDeclType instanceof UndefinedType) || (rightDeclType instanceof VoidType))))) || ((rightDeclType instanceof AnyType) && (!(leftDeclType instanceof VoidType))))) {
+        if ((((leftDeclType instanceof UndefinedType) || ((leftDeclType instanceof NullType) && (!(((rightDeclType instanceof UndefinedType) || (rightDeclType instanceof VoidType)) || (rightDeclType instanceof TypeVariable))))) || ((rightDeclType instanceof AnyType) && (!((leftDeclType instanceof VoidType) || (leftDeclType instanceof TypeVariable)))))) {
           /* true */
           if (!true) {
             sneakyThrowRuleFailedException("true");

commit 839790985bfe38add96d644ae771da74ce0c5b67

IDE-1579: fix for a problem introduced by commit 477c8ca

plugins/eu.numberfour.n4js/src/eu/numberfour/n4js/typesystem/TypeSystemHelper.xtend
@@ -432,4 +432,14 @@ def StructuralTypingComputer getStructuralTypingComputer() {
 		
 		return unionTRs
 	}
+
+	/**
+	 * Tells if the given type reference is or might possibly end up being type 'void'.
+	 * For example, this method will return <code>true</code> for a type variable iff 'void' would make a valid type
+	 * argument, i.e. iff <code>void</code> is a subtype of the type variable's explicitly declared or implicit upper
+	 * bound.
+	 */
+	def boolean isOrMightBeVoid(RuleEnvironment G, TypeRef typeRef) {
+		ts.subtypeSucceeded(G, G.voidTypeRef, resolveType(G, typeRef))
+	}
 }

plugins/eu.numberfour.n4js/src/eu/numberfour/n4js/typesystem/n4js.xsemantics
@@ -1223,8 +1223,8 @@ from {
 	} else if (leftDeclType.eIsProxy || rightDeclType.eIsProxy) {
 		true;
 	} else if (leftDeclType instanceof UndefinedType
-				|| (leftDeclType instanceof NullType && !(rightDeclType instanceof UndefinedType || rightDeclType instanceof VoidType || rightDeclType instanceof TypeVariable))
-				|| (rightDeclType instanceof AnyType && !(leftDeclType instanceof VoidType || leftDeclType instanceof TypeVariable))) {
+				|| (leftDeclType instanceof NullType && !(rightDeclType instanceof UndefinedType || rightDeclType instanceof VoidType) && !typeSystemHelper.isOrMightBeVoid(G, right))
+				|| (rightDeclType instanceof AnyType && !(leftDeclType instanceof VoidType) && !typeSystemHelper.isOrMightBeVoid(G, right))) {
 		true
 	} else if (leftDeclType instanceof VoidType
 				|| rightDeclType instanceof VoidType) {

plugins/eu.numberfour.n4js/xsemantics-gen/eu/numberfour/n4js/xsemantics/InternalTypeSystem.java
@@ -3723,7 +3723,7 @@ public class InternalTypeSystem extends XsemanticsRuntimeSystem {
           sneakyThrowRuleFailedException("true");
         }
       } else {
-        if ((((leftDeclType instanceof UndefinedType) || ((leftDeclType instanceof NullType) && (!(((rightDeclType instanceof UndefinedType) || (rightDeclType instanceof VoidType)) || (rightDeclType instanceof TypeVariable))))) || ((rightDeclType instanceof AnyType) && (!((leftDeclType instanceof VoidType) || (leftDeclType instanceof TypeVariable)))))) {
+        if ((((leftDeclType instanceof UndefinedType) || (((leftDeclType instanceof NullType) && (!((rightDeclType instanceof UndefinedType) || (rightDeclType instanceof VoidType)))) && (!this.typeSystemHelper.isOrMightBeVoid(G, right)))) || (((rightDeclType instanceof AnyType) && (!(leftDeclType instanceof VoidType))) && (!this.typeSystemHelper.isOrMightBeVoid(G, right))))) {
           /* true */
           if (!true) {
             sneakyThrowRuleFailedException("true");