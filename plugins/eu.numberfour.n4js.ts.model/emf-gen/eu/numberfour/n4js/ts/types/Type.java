b7bbb70fe816bbd03149472bd4d57eae9af89a31
Jens von Pilgrim <Jens.von.Pilgrim@numberfour.eu>
2016-10-21 10:33:04 +0200

    IDE-2400: added version and declared version in type and containertype

--- plugins/eu.numberfour.n4js.ts.model/emf-gen/eu/numberfour/n4js/ts/types/Type.java
+++ plugins/eu.numberfour.n4js.ts.model/emf-gen/eu/numberfour/n4js/ts/types/Type.java
@@ -209,4 +209,18 @@ public interface Type extends TExportableElement, TAnnotableElement {
 	 */
 	String getTypeAsString();
 
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * Returns the version of the type (declaration). This is 0 by default for all N4JS types, as this
+	 * feature is only supported by derived languages (such as N4IDL). Subclasses may override this method,
+	 * returning the declared version instead.
+	 * <!-- end-model-doc -->
+	 * @model kind="operation" unique="false"
+	 *        annotation="http://www.eclipse.org/emf/2002/GenModel body='return 0;'"
+	 * @generated
+	 */
+	int getVersion();
+
 } // Type
