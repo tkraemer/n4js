d0f19f10e52629e3683057d7b5382222d852f66d
Quang Tran <quang.tran@numberfour.eu>
2016-11-15 09:30:25 +0100

    GH-272: Fix dirty state bug but having an ovrriden format method

--- plugins/eu.numberfour.n4js.ui/src/eu/numberfour/n4js/ui/formatting2/FixedContentFormatter.java
+++ plugins/eu.numberfour.n4js.ui/src/eu/numberfour/n4js/ui/formatting2/FixedContentFormatter.java
@@ -12,12 +12,15 @@ package eu.numberfour.n4js.ui.formatting2;
 
 import java.util.List;
 
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.ReplaceEdit;
 import org.eclipse.text.edits.TextEdit;
 import org.eclipse.xtext.formatting2.regionaccess.ITextRegionAccess;
 import org.eclipse.xtext.formatting2.regionaccess.ITextReplacement;
 import org.eclipse.xtext.ui.editor.formatting2.ContentFormatter;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 
 /**
  * Use a single {@link ReplaceEdit} instead of a {@link MultiTextEdit} for performance reasons.
@@ -31,12 +34,30 @@ public class FixedContentFormatter extends ContentFormatter {
 	@Override
 	protected TextEdit createTextEdit(List<ITextReplacement> replacements) {
 		if (replacements.isEmpty()) {
-			// return new ReplaceEdit(0, 0, "");
-			return null; // Return null, otherwise even if there are no changes, e.g. formatting, editor is in dirty
-							// state
+			return new ReplaceEdit(0, 0, "");
 		}
 		ITextRegionAccess regionAccess = replacements.get(0).getTextRegionAccess();
 		String newDocument = regionAccess.getRewriter().renderToString(replacements);
 		return new ReplaceEdit(0, regionAccess.regionForDocument().getLength(), newDocument);
+	}
+
+	/**
+	 * This method makes sure that no changes are applied (no dirty state), if there are no changes. This fixes bug
+	 * GH-272
+	 */
+	@Override
+	public void format(IDocument document, IRegion region) {
+		IXtextDocument doc = (IXtextDocument) document;
+		TextEdit e = doc.priorityReadOnly(new FormattingUnitOfWork(doc, region));
+
+		if (e == null)
+			return;
+		if (e instanceof ReplaceEdit) {
+			ReplaceEdit r = (ReplaceEdit) e;
+			if ((r.getOffset() == 0) && (r.getLength() == 0) && (r.getText().isEmpty())) {
+				return;
+			}
+		}
+		super.format(document, region);
 	}
 }
