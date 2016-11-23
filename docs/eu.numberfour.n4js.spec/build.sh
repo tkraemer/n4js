85e18c3ac3fe4527c76badad9e700a031f24de27
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2016-11-23 11:24:50 +0100

    Revert "GH-292: support for new inheritance hierarchies in IterableN extraction"
    This reverts commit 4ef804f085397a75e92ca8cef5c112e2cf652d7a, that was accidentally committed/pushed to master. Sorry :)
    
    IDE-2439 removing PDF
    
    IDE-2439 renaming example references
    
    IDE-2439 fixing pdf build
    
    IDE-2439 fixing xrefs
    
    IDE-2439 editing xrefs
    
    IDE-2439 updating xrefs

--- docs/eu.numberfour.n4js.spec/build.sh
+++ docs/eu.numberfour.n4js.spec/build.sh
@@ -12,12 +12,12 @@ GEN_FOLDER=generated-docs/html
 rm -rf ./$GEN_FOLDER/; mkdir -p ./$GEN_FOLDER/
 
 echo INFO: Copying resources to ./$GEN_FOLDER/
-cp -r scripts styles images ./$SRC_FOLDER/chapters ./$GEN_FOLDER/
+cp -r styles images scripts ./$GEN_FOLDER/
 
 echo INFO: AsciiSpec Generating HTML
 
 ####################### Build HTML for gh-pages #######################
-asciispec -D $GEN_FOLDER/ N4JSSpec.adoc  
+asciispec -a data-uri=true -D $GEN_FOLDER/ N4JSSpec.adoc 
 
 echo INFO: AsciiSpec HTML conversion Done
 
