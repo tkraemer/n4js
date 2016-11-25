1238d1a054e5889d59e056e7bf44e41ecd5b9fcd
Quang Tran <quang.tran@numberfour.eu>
2016-11-25 17:00:34 +0100

    IDE-2415: Fix naming errors of N4JS Xpect tests suite

--- tests/eu.numberfour.n4js.xpect.tests/src/eu/numberfour/n4js/tests/N4JSXpectTest.java
+++ tests/eu.numberfour.n4js.xpect.tests/src/eu/numberfour/n4js/tests/N4JSXpectTest.java
@@ -47,6 +47,6 @@ import eu.numberfour.n4js.xpect.validation.suppression.SuppressIssuesSetup;
 @RunWith(XpectRunner.class)
 @XpectTestFiles(relativeTo = FileRoot.PROJECT, baseDir = "model", fileExtensions = { "xt" })
 @XpectImport(SuppressIssuesSetup.class)
-public class N4JSSpectTest {
+public class N4JSXpectTest {
 	//
 }
