f2639e11a7a7329f221889539fa9990784ee891f
Bsmith <brian.smith@numberfour.eu>
2017-01-24 14:06:23 +0100

    IDE-2466 updating build script to use new AsciiSpec behaviour

--- docs/eu.numberfour.n4js.spec/build.sh
+++ docs/eu.numberfour.n4js.spec/build.sh
@@ -11,15 +11,12 @@ GEN_FOLDER=generated-docs/html
 
 rm -rf ./$GEN_FOLDER/; mkdir -p ./$GEN_FOLDER/
 
-echo INFO: Copying resources to ./$GEN_FOLDER/
+# Copy resources to ./$GEN_FOLDER/
 cp -r styles images scripts ./$GEN_FOLDER/
 
-echo INFO: AsciiSpec Generating HTML
 
-####################### Build HTML for gh-pages #######################
+############## Build HTML for gh-pages #############
 asciispec -a data-uri=true -a stylesheet=foundation.css -D $GEN_FOLDER/ N4JSSpec.adoc 
-
-echo INFO: AsciiSpec HTML conversion Done
 
 # running "./build.sh -p" (preview) will skip PDF and launch index.html
 if [ "${1}" == "--preview" ] || [ "${1}" == "-p" ]; then
@@ -28,14 +25,8 @@ exit 0
 fi
 
 ####### Build PDF for gh-pages download #######
-echo INFO: AsciiSpec Generating PDF
-asciispec -fop N4JSSpec.adoc
+asciispec -b docbook N4JSSpec.adoc
+fopub N4JSSpec.xml 
 mv N4JSSpec.pdf ./$GEN_FOLDER/
-
-# running "./build.sh -l" will launch N4JSSpec.html after build
-if [ "${1}" == "--launch" ] || [ "${1}" == "-l" ]; then
-open ./$GEN_FOLDER/N4JSSpec.html
-exit 0
-fi
 
 echo DONE: AsciiSpec conversion finished.
