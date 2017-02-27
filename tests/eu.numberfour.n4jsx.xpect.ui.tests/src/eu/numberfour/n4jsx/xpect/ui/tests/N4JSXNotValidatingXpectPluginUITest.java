5a315f82d1c9392f96076a9df5a5037d5cd31ac9
Jakub Siberski <jakub.siberski@numberfour.eu>
2017-02-27 19:12:26 +0100

    IDE-2535 add Xpect Plugin tests for N4JX

---
+++ tests/eu.numberfour.n4jsx.xpect.ui.tests/src/eu/numberfour/n4jsx/xpect/ui/tests/N4JSXNotValidatingXpectPluginUITest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2016 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package eu.numberfour.n4jsx.xpect.ui.tests;
+
+import org.junit.runner.RunWith;
+import org.xpect.XpectImport;
+import org.xpect.lib.XpectTestResultTest;
+import org.xpect.runner.XpectRunner;
+import org.xpect.runner.XpectSuiteClasses;
+import org.xpect.runner.XpectTestFiles;
+import org.xpect.runner.XpectTestFiles.FileRoot;
+import org.xpect.xtext.lib.tests.ValidationTest;
+
+import eu.numberfour.n4js.xpect.config.Config;
+import eu.numberfour.n4js.xpect.config.VarDef;
+import eu.numberfour.n4js.xpect.config.XpEnvironmentData;
+import eu.numberfour.n4js.xpect.ui.methods.HyperlinkXpectMethod;
+import eu.numberfour.n4js.xpect.ui.methods.OrganizeImportXpectMethod;
+import eu.numberfour.n4js.xpect.ui.methods.ProposalXpectMethod;
+import eu.numberfour.n4js.xpect.ui.methods.contentassist.ContentAssistXpectMethod;
+import eu.numberfour.n4js.xpect.ui.methods.quickfix.QuickFixXpectMethod;
+import eu.numberfour.n4jsx.xpect.validation.suppression.SuppressIssuesSetup;
+
+/**
+ * Plugin for proposal test. This plugin turns <b>off</b> validation in xpect-tests by default since most input files
+ * are invalid before applying and only some become valid after applying a proposal.
+ */
+@XpectSuiteClasses({
+		// LinkingTest.class,
+		// N4JSTypeSystemXpectTestFragment.class,
+		// NoerrorsValidationTestFragment.class,
+		// PositionAwareScopingXpectTestFragment.class,
+		// ResourceDescriptionTest.class,
+		ValidationTest.class,
+		QuickFixXpectMethod.class,
+		ProposalXpectMethod.class,
+		HyperlinkXpectMethod.class,
+		ContentAssistXpectMethod.class,
+		XpectTestResultTest.class,
+		OrganizeImportXpectMethod.class
+})
+@XpectImport({ Config.class, VarDef.class, XpEnvironmentData.class, SuppressIssuesSetup.class })
+@RunWith(XpectRunner.class)
+@XpectTestFiles(relativeTo = FileRoot.PROJECT, baseDir = "model_nonvalidating", fileExtensions = "xt")
+public class N4JSXNotValidatingXpectPluginUITest {
+	//
+
+}
