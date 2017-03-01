1cbdb51d11bd65ae4a38ceab2696e16cae76d129
Bsmith <brian.smith@numberfour.eu>
2017-03-01 15:07:38 +0100

    IDE-2457 clean up and add comments to build script

--- docs/eu.numberfour.n4js.spec/build.sh
+++ docs/eu.numberfour.n4js.spec/build.sh
@@ -1,57 +1,61 @@
 #!/bin/bash
-## Syntax for following set command:
+
 # -e  == exit immediately
 # -x  == enable debug. (+x for disable)
 # -v  == Print shell input lines as they are read.
 set -e +x -v
 
-########## Directory Locations ###########
-# output folder:
-GEN_FOLDER=generated-docs/
+# Which document to convert 
+SPEC="N4JSSpec.adoc"
+
+# Document attributes
 ATTRS="-a stylesheet=foundation.min.css -a docinfodir=headers "
-rm -rf ./$GEN_FOLDER/; mkdir -p ./$GEN_FOLDER/
 
-# Copy resources to ./$GEN_FOLDER/
-cp -r styles images scripts chapters ./$GEN_FOLDER/
+############## Build HTML #############
 
-
-############## Build HTML for gh-pages #############
+# if Jenkins is building, use the following params
 if [ "${1}" == "--jenkins" ]; then
-	
-	# which document to convert 
-	SPEC="N4JSSpec.adoc"
-	
-	# the output directory
-	GEN_FOLDER="../generated-docs/spec"
 
+	# Specify & Clean output directory
+	GEN_FOLDER="../generated-docs/spec"
 	rm -rf $GEN_FOLDER; mkdir -p $GEN_FOLDER
+
 	# Copy resources to ./$GEN_FOLDER/
 	cp -r ./styles ./images ./scripts ./chapters $GEN_FOLDER/
 
+	####### Build HTML Spec #######
 	asciispec $ATTRS -D $GEN_FOLDER $SPEC
-	 
-	asciispec -b docbook -D $GEN_FOLDER $SPEC 
-	fopub $GEN_FOLDER/N4JSSpec.xml 
-	rm $GEN_FOLDER/N4JSSpec.xml
+	
+	####### Build PDF via docbook toolchain #######
+	asciispec -b docbook -D $GEN_FOLDER $SPEC && fopub $GEN_FOLDER/N4JSSpec.xml 
+
+	##### EXIT ##### 
 	exit 0
+
+# otherwise, use these params
+else
+	# Specify & Clean output directory
+	GEN_FOLDER=generated-docs
+	rm -rf ./$GEN_FOLDER/; mkdir -p ./$GEN_FOLDER/
+
+	# Copy resources to ./$GEN_FOLDER/
+	cp -r styles images scripts chapters ./$GEN_FOLDER/
+
+	####### Build HTML Spec #######
+	asciispec $ATTRS -D $GEN_FOLDER N4JSSpec.adoc 
+
+	# running "./build.sh -p" (preview) will skip PDF build and launch N4JSSpec.html
+	if [ "${1}" == "--preview" ] || [ "${1}" == "-p" ]; then
+		open ./$GEN_FOLDER/N4JSSpec.html
+		exit 0
+	fi
 fi
 
-asciispec $ATTRS -D $GEN_FOLDER N4JSSpec.adoc 
-
-# running "./build.sh -p" (preview) will skip PDF and launch index.html
-if [ "${1}" == "--preview" ] || [ "${1}" == "-p" ]; then
-open ./$GEN_FOLDER/N4JSSpec.html
-exit 0
-fi
-
-####### Build PDF for gh-pages download #######
-asciispec -b docbook N4JSSpec.adoc
-fopub N4JSSpec.xml 
+####### Build PDF via docbook toolchain #######
+asciispec -b docbook N4JSSpec.adoc && fopub N4JSSpec.xml 
 mv N4JSSpec.pdf ./$GEN_FOLDER/
 
 # Clean unwanted adoc/graffle files and delete empty subdirectories
 pushd ./$GEN_FOLDER/chapters
 	rm -rf **/*.adoc && rm -rf **/**/*.graffle &&	find . -type d -empty -print
 popd
-
-echo DONE: AsciiSpec conversion finished.
